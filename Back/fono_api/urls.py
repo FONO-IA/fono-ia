5e01d57b15d64fb03e274f28456386600bef2d63
thiago-arauj <thiago.araujo.rodrigues.mendes@gmail.com>
2026-03-20 19:13:04 -0300

    feat: created back structure

---
+++ Back/fono_api/urls.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+
+# Imports do Swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Configuração do Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Taxonomia de Avaliação API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('api/v1/', include(core_router.urlpatterns)), exemplo
+
+    # Swagger URLs
+    path('docs/', schema_view.with_ui(
+        'swagger',
+        cache_timeout=0
+        ),
+        name='schema-swagger-ui'
+    ),
+    path('redoc/', schema_view.with_ui(
+        'redoc',
+        cache_timeout=0
+        ),
+        name='schema-redoc'
+    ),
+
+    # Redirecionamentos para documentação
+    path('swagger/', RedirectView.as_view(url='/docs/', permanent=True)),
+    path('doc/', RedirectView.as_view(url='/docs/', permanent=True)),
+    path('documentation/', RedirectView.as_view(url='/docs/', permanent=True)),
+    path('api-docs/', RedirectView.as_view(url='/docs/', permanent=True)),
+    path('redocs/', RedirectView.as_view(url='/redoc/', permanent=True)),
+    path('api-redoc/', RedirectView.as_view(url='/redoc/', permanent=True)),
+]
