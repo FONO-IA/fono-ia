e2af5596a8a34a000ade1cb0fb3065389f45a4fb
Thiago <thiagoports@hotmail.com>
2026-04-15 19:57:17 -0300

    fix: import and router.register

--- Back/apps/responsavel/api/v1/router.py
+++ Back/apps/responsavel/api/v1/router.py
@@ -1,10 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from apps.core.api.v1.viewsets import ResponsavelViewSet
+from apps.responsavel.api.v1.viewsets import ResponsavelViewSet
 
 router = DefaultRouter()
 
 router.register(
-    r'responsavel', ResponsavelViewSet, basename='responsavel'
+    r'responsaveis', ResponsavelViewSet, basename='responsavel'
 )
 
 urlpatterns = router.urls
