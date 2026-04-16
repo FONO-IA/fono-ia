07875108de1450960e4b2fd4f814254c808a46fd
Paulo Cleidson <167731152+Paulo-Cleidson@users.noreply.github.com>
2026-04-16 15:13:16 -0300

    fix: refactor

--- Back/apps/exercicio/api/v1/serializer.py
+++ Back/apps/exercicio/api/v1/serializer.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from apps.exercicio.models import Exercicio
+from apps.exercicio.models import Exercicio, ConteudoExercicio
+
+
+class ConteudoExercicioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConteudoExercicio
+        fields = ["id", "texto", "instrucao"]
 
 
 class ExercicioSerializer(serializers.ModelSerializer):
+    conteudos = ConteudoExercicioSerializer(many=True)
+
     class Meta:
         model = Exercicio
-        fields = '__all__'
+        fields = "__all__"
+
+    def create(self, validated_data):
+        conteudos_data = validated_data.pop("conteudos", [])
+        exercicio = Exercicio.objects.create(**validated_data)
+
+        for conteudo in conteudos_data:
+            ConteudoExercicio.objects.create(
+                exercicio=exercicio,
+                **conteudo
+            )
+
+        return exercicio
