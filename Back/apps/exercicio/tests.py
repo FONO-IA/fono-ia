91019cb64d725e40f3bfec83fe3ef70b8d59e050
Thiago <thiagoports@hotmail.com>
2026-04-23 13:28:33 -0300

    feat: tests-exercicio

--- Back/apps/exercicio/tests.py
+++ Back/apps/exercicio/tests.py
@@ -5,11 +5,11 @@ from .models import Exercicio
 class ExercicioModelTest(TestCase):
     def test_criacao_exercicio(self):
         exercicio = Exercicio.objects.create(
-            nivel='Fácil',
+            nivel='FAC',
             categoria='Leitura',
             conteudo='Texto de exemplo',
             objetivo='Melhorar leitura',
             instrucao='Leia o texto em voz alta.'
         )
-        self.assertEqual(exercicio.nivel, 'Fácil')
+        self.assertEqual(exercicio.nivel, 'FAC')
         self.assertEqual(exercicio.categoria, 'Leitura')
