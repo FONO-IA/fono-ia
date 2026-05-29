952ba41e294ecc5d6931cf536e2dcf1476a9576a
thiago-arauj <thiago.araujo.rodrigues.mendes@gmail.com>
2026-05-29 20:07:53 -0300

    fix: fixed media error

--- Back/fono_api/settings.py
+++ Back/fono_api/settings.py
@@ -196,8 +196,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Media files (uploads)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'https://fonoia-django.duckdns.org/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
