607183119df97025fa904005b9a295ed5c6e694d
thiago-arauj <thiago.araujo.rodrigues.mendes@gmail.com>
2026-05-29 22:23:21 -0300

    feat: added link to cors

--- VE_api/app/main.py
+++ VE_api/app/main.py
@@ -46,7 +46,10 @@ app = FastAPI(
 # Configura CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        'https://fono-ia.vercel.app',
+        'http://localhost:5173',
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
